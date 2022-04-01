56c985dc1c72dc64085b1f54856a30c89edf69a3
Stanislav Stamatov <stanislav.stamatov45@abv.bg>
2022-04-02 01:42:09 +0300

    The solutions for the problems from the first exercise.

---
+++ FirstExercise/FishTank/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace FishTank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int length = int.Parse(Console.ReadLine());
+            int width = int.Parse(Console.ReadLine());
+            int height = int.Parse(Console.ReadLine());
+            double percent = double.Parse(Console.ReadLine());
+
+            int volume = length * width * height;
+            double volumeInCubics = volume * 0.001;
+            double percentage = percent / 100;
+            double litresNeeded = volumeInCubics * (1 - percentage);
+
+            Console.WriteLine(litresNeeded);
+        }
+    }
+}
