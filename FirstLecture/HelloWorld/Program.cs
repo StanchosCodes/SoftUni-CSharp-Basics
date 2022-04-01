c61e654e3e7a12ffb5addf2229a7f597a14a7094
Stanislav Stamatov <stanislav.stamatov45@abv.bg>
2022-04-02 01:40:31 +0300

    The solutions of the problems from the first lecture.

---
+++ FirstLecture/HelloWorld/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //How to start the program - ctrl + f5
+            Console.WriteLine("Hello SoftUni");
+        }
+    }
+}
