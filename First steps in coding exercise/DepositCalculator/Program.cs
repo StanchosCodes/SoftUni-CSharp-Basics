0ea9b8c5b9372f3fae886a7dfa61f07d97cc821d
Stanislav Stamatov <stanislav.stamatov45@abv.bg>
2022-04-03 11:56:13 +0300

    Changed the names of first lecture and exercise folders.

---
+++ First steps in coding exercise/DepositCalculator/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace DepositCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double deposit = double.Parse(Console.ReadLine());
+            int deadline = int.Parse(Console.ReadLine());
+            double percent = double.Parse(Console.ReadLine());
+
+            double risedPercent = deposit * (percent / 100);
+            double monthlyPercent = risedPercent / 12;
+            double sum = deposit + deadline * monthlyPercent;
+
+            Console.WriteLine(sum);
+        }
+    }
+}
---
+++ First steps in coding/ProjectsCreation/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ProjectsCreation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int TIME = 3;
+            string name = Console.ReadLine();
+            int num = int.Parse(Console.ReadLine());
+            int timeNeeded = num * TIME;
+            Console.WriteLine($"The architect {name} will need {timeNeeded} hours to complete {num} project/s.");
+        }
+    }
+}
